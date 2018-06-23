c83e0107b7f62ad81807d3145b69bee31f6b6bcd
Dillorom Abdulloeva <dabdulloeva@gmail.com>
2018-06-23 17:42:11 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -69,14 +69,6 @@ class Student
   end
 
   def self.find_by_name(name)
-    sql = <<-SQL
-      SELECT *
-      FROM students
-      WHERE name = ?
-      LIMIT 1
-    SQL
-    DB[:conn].execute(sql,name).map do |row|
-      self.new_from_db(row)
-    end.first
+    self.all.find {|a| a.name == name}
   end
 end
