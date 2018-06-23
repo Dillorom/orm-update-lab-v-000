8e6a041f1155c57cf7684a6f41f595cc11f34fbf
Dillorom Abdulloeva <dabdulloeva@gmail.com>
2018-06-23 18:25:23 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -44,7 +44,7 @@ class Student
       sql = "UPDATE students SET name = ?, grade = ?, id = ?"
       DB[:conn].execute(sql, self.name, self.grade, self.id)
     end
-  
+
   def self.create_table
     sql = <<-SQL
       CREATE TABLE IF NOT EXISTS students (
@@ -73,6 +73,6 @@ class Student
   def self.find_by_name(name)
     sql = "SELECT * FROM students WHERE name = ?"
     result = DB[:conn].execute(sql, name)[0]
-    Student.new(result[0], result[1], result[2]
+    Student.new(result[0], result[1], result[2])
   end
 end
