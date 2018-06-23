9dea26818fa2cd26a7126a9bb0732125658592ec
Dillorom Abdulloeva <dabdulloeva@gmail.com>
2018-06-23 18:34:54 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -64,11 +64,10 @@ class Student
   end
 
   def self.new_from_db(row)
-     
-     new_student.id = row[0]
-     new_student.name = row[1]
-     new_student.grade = row[2]
-     new_student = self.new(name, grade, id = nil)
+     id = row[0]
+     name = row[1]
+     grade = row[2]
+    self.new(name, grade, id)
   end
 
   def self.find_by_name(name)
